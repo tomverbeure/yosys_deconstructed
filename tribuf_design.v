8deaee3b31df5f0bfea8f2e9a4e80c31c4dc1db3
Tom Verbeure <hombre+github@gmail.com>
2019-07-04 18:37:11 -0700

    Better tri-buf example.

--- tribuf_design.v
+++ tribuf_design.v
@@ -1,16 +1,24 @@
 
-module tribuf_design(a, b, z0, z1, z2);
+module tribuf_design(clk, en_a, a, en_b, b, z0, z1, z2);
 
-	input       a;
-	input       b;
+    input       clk;
 
-	output      z0, z1; 
+	input       en_a, a;
+	input       en_b, b;
+
+	output      z0; 
+    output reg  z1; 
     inout       z2;
 
-    assign z0 = a ? 1'b1 : 1'bz;
+    wire        bus;
 
-    assign z1 = a ? 1'b1 : 1'bz;
-    assign z1 = b ? 1'b1 : 1'bz;
+    assign z0 = en_a ? a : 1'bz;
+
+    assign bus = en_a ? a : 1'bz;
+    assign bus = en_b ? b : 1'bz;
+
+    always @(posedge clk) 
+        z1 <= bus; 
 
     assign z2 = a;
 
